d97f3e82455e49248f3dedd76e7bcf59a6edb0b0
nurullahgungor <ngngrx1@gmail.com>
2024-05-13 18:26:10 +0300

    feat : add the repository's

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use Illuminate\Contracts\View\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +30,9 @@ Route::get('/user/[name]', [UserController::class, 'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::get('/about', function(){
+    return view();
+});
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
